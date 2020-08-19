6211ffcb74491bb2837ca4babc3da77485599c04
Fab <ensinergia@gmail.com>
2020-08-18 22:01:04 -0600

    Agregar URLs y views y models a comunidades

--- buses/comunidades/models.py
+++ buses/comunidades/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Comunidad(models.Model):
+    nombre = models.CharField(max_length=32)
+    resena = models.TextField('Reseña de la comunidad')
+    url = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.nombre
