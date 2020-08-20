7aa7872613eef9309ee40634bee21781d28e2538
Fab <ensinergia@gmail.com>
2020-08-19 22:19:23 -0600

    Ajustes menores

--- buses/comunidades/models.py
+++ buses/comunidades/models.py
@@ -6,6 +6,9 @@ class Comunidad(models.Model):
     nombre = models.CharField(max_length=32)
     resena = models.TextField('Reseña de la comunidad')
     url = models.CharField(max_length=32)
+    poblacion = models.IntegerField(default=0)
+    area = models.IntegerField(default=0)
+    codigo_postal = models.IntegerField(default=0)
 
     def __str__(self):
         return self.nombre
--- buses/rutas/models.py
+++ buses/rutas/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Ruta(models.Model):
+    route_id = models.IntegerField()
+    agency_id = models.CharField(max_length=16)
+    route_short_name = models.CharField(max_length=16)
+    route_long_name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.route_short_name
