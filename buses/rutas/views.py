b218189e7687fe6d8deac055ee3780585f5a9992
Anyelo <yeloanlo@gmail.com>
2020-08-20 22:17:34 -0600

    Avance en despliegue de horarios en base a los modelos GTFS

--- buses/rutas/views.py
+++ buses/rutas/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, render
-from .models import Route
+from .models import Route, Trip, Stop, StopTime
+from datetime import datetime # For current time
 
 def rutas(request):
     return render(request, 'rutas.html')
@@ -7,8 +8,23 @@ def rutas(request):
 def ruta(request, url_ruta):
     route = get_object_or_404(Route, url=url_ruta)
 
+    stop = get_object_or_404(Stop, stop_id="terminal")
+
+    trip = Trip.objects.filter(route=route)
+
+    stop_times = StopTime.objects.filter(trip=trip[0])
+
+    print("STOP TIMES = ", stop_times)
+
+    # Get current time to display it in the template
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+
     context = {
         'route': route,
+        'current_time': current_time,
+        'stop': stop,
+        'stop_times': stop_times,
     }
 
     return render(request, 'ruta.html', context)
