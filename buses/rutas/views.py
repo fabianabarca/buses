56279be1fdee13aef4ff0c11fc974a63658d5aa4
Anyelo <yeloanlo@gmail.com>
2020-08-25 13:48:02 -0600

    Agregada funcionalidad Próximo Bus a la App Rutas

--- buses/rutas/views.py
+++ buses/rutas/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import get_object_or_404, render
 from .models import Route, Trip, Stop, StopTime
 from datetime import datetime # For current time
 
+""" Function to execute when user goes to /rutas """
 def rutas(request):
     return render(request, 'rutas.html')
 
+""" Function to execute when user goes to specific Route (E.g. /rutas/sangabriel o /rutas/acosta) """
 def ruta(request, url_ruta):
     # Get the route depending on the url
     route = get_object_or_404(Route, url=url_ruta)
@@ -25,6 +27,11 @@ def ruta(request, url_ruta):
     # Get current time to display it in the template
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
+    
+    # Get the next 3 buses list from the Route to San Jose
+    bus_listRoute = nextBuses(stop_times_Route, now)
+    # Get the next 3 buses list from San Jose to the Route
+    bus_listSJ = nextBuses(stop_times_SJ, now)
 
     context = {
         'route': route,
@@ -32,6 +39,44 @@ def ruta(request, url_ruta):
         'stop': stop,
         'stop_times_list': stop_times_list,
         'stop_times_SJ_last': stop_times_SJ.last(), # To send the last departure from "San José" in the cases that the last row of the schedule have only a stop time for "San José"
+        'bus_listRoute': bus_listRoute, # Next 3 buses
+        'bus_listSJ': bus_listSJ, # Next 3 buses
     }
 
     return render(request, 'ruta.html', context)
+
+""" Function that calculates the next 3 buses and return them in a list (list of datetimes) """
+def nextBuses(stop_times, current_time):
+    bus_list = [] # List to return with the next buses
+
+    stop_times_list = list(stop_times) # Convert the scehule given to a list
+    iterator = iter(stop_times_list) # Get an iterator for the stop_times_list
+    next(iterator)  # Get the first element in the list (to get next elements in the for cycle)
+    for item in stop_times_list:
+        if current_time.hour == item.departure_time.hour:
+            if current_time.minute < item.departure_time.minute:
+                try:
+                    bus_list.append(item.departure_time)
+                    bus_list.append(next(iterator).departure_time)
+                    bus_list.append(next(iterator).departure_time)
+                except:
+                    break
+            else:
+                try:
+                    bus_list.append(next(iterator).departure_time)
+                    bus_list.append(next(iterator).departure_time)
+                    bus_list.append(next(iterator).departure_time)
+                except:
+                    break
+        if current_time.hour + 1 == item.departure_time.hour:
+            try:
+                bus_list.append(item.departure_time)
+                bus_list.append(next(iterator).departure_time)
+                bus_list.append(next(iterator).departure_time)
+            except:
+                break
+        try:
+            next(iterator)
+        except:
+            break
+    return bus_list
