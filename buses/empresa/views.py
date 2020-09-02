69a5b5666575d1519564e7cf8721f9f0aca74401
Marisol Zeledón <mari.sol_17@hotmail.com>
2020-09-01 18:59:18 -0600

    Texto enriquecido en nosotros

--- buses/empresa/views.py
+++ buses/empresa/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+from .models import Nota
 
 # Create your views here.
 
 def empresa(request):
-    return render(request, 'empresa.html')
+    empresa = Nota.objects.all()
+    contexto = {
+        'empresa': empresa
+    }
+    return render(request, 'empresa.html', contexto)
 
 def personal(request):
     return render(request, 'personal.html')
+
+
+
