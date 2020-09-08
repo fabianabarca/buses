90a908065b8a741a97d4d0869a49ab6c70f6f3fe
Fabián Abarca <fabianabarca@users.noreply.github.com>
2020-09-08 16:21:18 -0600

    Listo

--- buses/buses/urls.py
+++ buses/buses/urls.py
@@ -29,5 +29,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
+#Agregamos a urlpatterns la ruta para encontrar los elementos de MEDIA
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 urlpatterns += static(settings.STATIC_URL,
                         document_root=settings.STATICFILES_DIRS)
