98480a613bd70d8f06bab8c3f61fb6466c69d410
Fab <ensinergia@gmail.com>
2020-09-09 18:38:04 -0600

    Matar el template

--- buses/template/html/js/template-custom/js/template-custom.js
+++
@@ -1,9 +0,0 @@
-// Sticky menu
-$('#header').sticky();
-
-// Sticky menu
-$('#header-2, #header-3').sticky();
-// Preloader
-$(window).on('load', function(e) {
-	$('body').removeClass('loading');
-});
--- buses/template/html/js/ytplayer/elementvideo-custom.js
+++
@@ -1,5 +0,0 @@
-var myPlayer;
-        jQuery(function () {
-            var isIframe=function(){var a=!1;try{self.location.href!=top.location.href&&(a=!0)}catch(b){a=!0}return a};
-            myPlayer = jQuery("#video").YTPlayer();
-        });
