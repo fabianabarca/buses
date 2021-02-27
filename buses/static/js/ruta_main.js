0a04b9a225e35e2da51fa0c1abcf137d22f29b19
Jeancarlo Hidalgo <jeancahu@gmail.com>
2021-02-27 03:12:23 -0600

    add: implementación filtro de horas próximas válidas

--- buses/static/js/ruta_main.js
+++ buses/static/js/ruta_main.js
@@ -2,46 +2,63 @@
 // Cargamos el template en la sección de hora para que sea usado por Vue3
 document.getElementById("DigitalCLOCK").innerText = "{{ hora }}";
 
-// Cargamos la App Vue, estática en el server, dinámica en frontend
-fetch('/static/ruta_vue_app_proximobus.html').then(
-    response => {
-        switch (response.status) {
-        case 200: // OK
-            response.text().then( data =>{
-                // document.getElementById(
-                //     "proximo_bus_app").innerHTML = data;
-                console.log("TODO: sustituir el innerHTML con el del template");
-            });
-            break;
-        case 404: // Not found
-            console.log('Not Found');
-            break;
-        }
-    }
-);
-
-// // Funciones utilitarias
-function proximos_buses () {
-    console.log("proximo_bus_list");
-}
-
 // // Main Vue app
-const app = Vue.createApp({
+const ruta_app = Vue.createApp({
     data () {
         return {
             hello: "world",
-            hora: 0,
+            hora: "-",
+            tiempo_en_minutos: 0, // Con este elemento se recorren los arreglos de horarios
+            desde_sanjose: [], //WARN: debe estar declarado anteriormente en el document
+            hacia_sanjose: [],
         };
     },
     mounted () { // Estos metodos ejecutan en todo momento
-        setInterval(() => {
+        setInterval(() => { // Lógica de la aplicación HORADIGITAL
             var time = new Date();
+            this.tiempo_en_minutos = time.getMinutes() + time.getHours()*60;
             this.hora = time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
         }, 1000); // Cada segundo refresca
+        setInterval(() => { // Lógica de proximobus
+
+            // FIXME: Esta parte no me gusta
+            var time = new Date();
+            this.desde_sanjose = [];
+            horario_desde_sanjose.filter(
+                value => value[0] > this.tiempo_en_minutos ) // Mayores a la hora
+                .slice(0,3) // Los siguientes 3
+                .forEach(element => {
+                    time.setHours(element[1]);
+                    time.setMinutes(element[2]);
+                    this.desde_sanjose.push(
+                        [
+                            time.toLocaleString(
+                                'en-US', { hour: 'numeric', minute: 'numeric', hour12: true }),
+                            element[3] // Ramal
+                        ]);
+                });
+            this.hacia_sanjose = [];
+            horario_hacia_sanjose.filter(
+                value => value[0] > this.tiempo_en_minutos ) // Mayores a la hora
+                .slice(0,3) // Los siguientes 3
+                .forEach(element => {
+                    time.setHours(element[1]);
+                    time.setMinutes(element[2]);
+                    this.hacia_sanjose.push(
+                        [
+                            time.toLocaleString(
+                                'en-US', { hour: 'numeric', minute: 'numeric', hour12: true }),
+                            element[3] // Ramal
+                        ]);
+                });
+            // END FIXME
+        }, 60000); // Cada 60 segundos refresca
     }
 });
-app.mount('#ruta_vue_app');
 
-// Hora
-// Próximo bus
+ruta_app.mount('#ruta_vue_app');
+
+// TODO:
+// Hora (listo)
+// Próximo bus (casi)
 // Mapa
