0ff514f7ce4043838b1a47754c4b0fadd1ab719c
Jeancahu <jeancahu@gmail.com>
2021-06-17 01:57:11 -0600

    add: versión del sitio como info de acerca🪛

--- buses/inicio/views.py
+++ buses/inicio/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from rutas.models import Route, FeedInfo
 from datetime import datetime
+from os import popen as bash
 
 # Create your views here.
 
@@ -11,13 +12,23 @@ def index(request):
     dias = ['lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado', 'domingo']
     fecha = [dias[ahora.weekday()], ahora.day, meses[ahora.month - 1], ahora.year]
     context = {
-        'rutas': rutas, # Lista de rutas  
-        'fecha': fecha,    
+        'rutas': rutas, # Lista de rutas
+        'fecha': fecha,
     }
     return render(request, 'index.html', context)
 
 def acerca(request):
-    return render(request, 'acerca.html')
+    try:
+        # La version del sitio se puede definir como el commit que se está utilizando, así como la fecha de creación del mismo.
+        version = bash("echo $(git branch --show-current) $( git reflog --pretty='%h -> %cD' | head -n 1 | cut -f -6 --delimiter=' ' )").read()
+    except:
+        # La version no funciona para Windows, está pensada para Ubuntu Server
+        version = 'UNKNOWN'
+
+    context ={
+        'version': version,
+    }
+    return render(request, 'acerca.html', context)
 
 def gtfs(request):
     informacion = FeedInfo.objects.get(pk=1)
