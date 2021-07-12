4a0b064b2e5d21b963f85a28d68f6a2be29d8fcf
Fab <ensinergia@gmail.com>
2021-07-12 11:28:23 -0600

    Creación de página de restricciones sanitarias por COVID-19 con información básica.

--- buses/inicio/urls.py
+++ buses/inicio/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('acerca/', views.acerca, name='acerca'),
     path('gtfs/', views.gtfs, name='gtfs'),
+    path('covid19/', views.covid19, name='covid19'),
     path('presentacion/', views.presentacion, name='presentacion'),
 ]
--- buses/inicio/views.py
+++ buses/inicio/views.py
@@ -6,12 +6,10 @@ from datetime import datetime
 
 def index(request):
     rutas = Route.objects.all()
-
     ahora = datetime.now()
     meses = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre']
     dias = ['lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado', 'domingo']
     fecha = [dias[ahora.weekday()], ahora.day, meses[ahora.month - 1], ahora.year]
-
     context = {
         'rutas': rutas, # Lista de rutas  
         'fecha': fecha,    
@@ -22,15 +20,14 @@ def acerca(request):
     return render(request, 'acerca.html')
 
 def gtfs(request):
-
     informacion = FeedInfo.objects.get(pk=1)
-
     context = {
         'informacion': informacion
     }
-
     return render(request, 'gtfs.html', context)
 
-def presentacion(request):
+def covid19(request):
+    return render(request, 'covid19.html')
 
+def presentacion(request):
     return render(request, 'presentacion.html')
