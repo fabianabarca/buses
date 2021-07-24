f1c172cb7cc18c1d6892fb523222c5d3018e0dc8
Fab <ensinergia@gmail.com>
2021-07-24 02:07:54 -0600

    Nuevas rutas (path) para STATIC_ROOT.

--- buses/buses/settings.py
+++ buses/buses/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ckeditor',
-    'shapeeditor' ## Sí esta línea te falla haz "pip install shapeeditor" para instalar la dependencia faltante
+    'shapeeditor' ## Si esta línea falla, hacer "pip install shapeeditor" para instalar la dependencia faltante
 ]
 
 MIDDLEWARE = [
@@ -122,14 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-
-# Url pública donde se almacenan las imágenes de noticias
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-
-# Url donde buscar los archivos multimedia
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Email configuration:
 
