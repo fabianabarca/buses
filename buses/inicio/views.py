3cc6c82a1c78962d6e444713080ea50bbf10656e
Jeancahu <jeancahu@gmail.com>
2021-09-20 06:02:16 -0600

    add: implementar la resolución automática de URL calendario restricciones

--- buses/inicio/views.py
+++ buses/inicio/views.py
@@ -2,8 +2,27 @@ from django.shortcuts import get_object_or_404, render
 from rutas.models import Route, FeedInfo, FareAttribute
 from datetime import datetime
 from os import popen as bash
+import requests, re
 
-# Create your views here.
+## Util functions
+def get_restriccion_calendar_url_from_mopt():
+    url = 'https://www.mopt.go.cr/wps/portal/Home/informacionrelevante/restriccion/'
+    response = requests.get(
+        url,
+        allow_redirects=True,
+        verify=False)
+
+    html = str(response.content)
+    images_url_list = re.findall('<img[^>]*[^>]*>', html)
+    restriccion_image_url = ''
+
+    for element in images_url_list:
+        if re.match('.*restricci.{2,16}sanitaria.*', element, flags=re.IGNORECASE):
+            restriccion_image_url = re.search('src="[^"]*', element)
+
+    return restriccion_image_url.group(0).replace('src="', 'https://www.mopt.go.cr')
+
+## Create your views here.
 
 def index(request):
     rutas = Route.objects.all()
@@ -49,7 +68,10 @@ def gtfs(request):
     return render(request, 'gtfs.html', context)
 
 def covid19(request):
-    return render(request, 'covid19.html')
+    context = {
+        'restric_calendar_url': get_restriccion_calendar_url_from_mopt()
+    }
+    return render(request, 'covid19.html', context)
 
 def presentacion(request):
     return render(request, 'presentacion.html')
