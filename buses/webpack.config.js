0a34c05a0d0704ee766c8aa594fbb60f1b3e234c
silvi0nsky <silviosalac@gmail.com>
2021-10-09 22:39:26 -0600

    remove:  static/vendor/jquery/, static/vendor/bootstrap add: jquery como dependencia chng: version dependencia bootstrap add: jquery y bootstrap configuracion para main bundle

--- buses/webpack.config.js
+++ buses/webpack.config.js
@@ -1,12 +1,33 @@
 
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-    entry: ['./static/js/ruta_main.js'],
+    entry: {
+        ruta_bundle: ['./static/js/ruta_main.js'],
+        main_bundle: ['bootstrap/dist/js/bootstrap.bundle.min.js']
+    },
     output: {
-        filename: 'bundle.js',
         path: path.resolve(__dirname, 'static/js/'),
     },
+    module: {
+        rules: [
+          {
+            test: require.resolve("jquery"),
+            loader: "expose-loader",
+            options: {
+                exposes: ["$", "jQuery"],
+            },
+          }
+        ]
+      },
     target: 'web',
     mode: 'production',
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery", 
+            jQuery: "jquery", 
+            jquery: "jquery"
+        }),
+    ]
 };
