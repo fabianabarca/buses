d4c5ba278fee6a161003b7a5f5e7751b9c009003
Jeancahu <jeancahu@gmail.com>
2021-10-31 17:05:09 -0600

    chg: separar la lógica de POST y GET en métodos diferentes

--- buses/contacto/views.py
+++ buses/contacto/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from django.core.mail import send_mail, BadHeaderError
 from django.http import HttpResponse, HttpResponseRedirect
+from django.views.decorators.http import require_GET, require_POST
 
 # Create your views here.
 
@@ -8,33 +9,32 @@ from rutas.models import Agency
 from .models import Pregunta
 from .form import Formulario
 
-def contacto(request):
+@require_POST
+def post_contact_form (request):
+    formulario = Formulario(request.POST)
+    if formulario.is_valid():
+        nombre = formulario.cleaned_data['nombre']
+        email = formulario.cleaned_data['email']
+        asunto = formulario.cleaned_data['asunto']
+        mensaje = formulario.cleaned_data['mensaje']
+        mensaje = mensaje + "\nEnviado por el usuario: " +\
+            nombre + '[' + email + ']'
 
-    # Estado del formulario
-    enviado = False
-
-    if request.method == 'GET':
-        formulario = Formulario()
+        try: # FIXME: implementar correctamente la configuración de SMTP usando correo del administrador que está en base de datos
+            send_mail(asunto, mensaje, email, ['tsgdumbacc@gmail.com'])
+            send_mail("Mensaje recibido", "Agredecemos su aporte.", 'tsgdumbacc@gmail.com', [email])
+        except BadHeaderError:
+            return HttpResponse('Configuración de correo inválida.')
     else:
-        formulario = Formulario(request.POST)
-        if formulario.is_valid():
-            nombre = formulario.cleaned_data['nombre']
-            email = formulario.cleaned_data['email']
-            asunto = formulario.cleaned_data['asunto']
-            mensaje = formulario.cleaned_data['mensaje']
-            mensaje = mensaje + "\nEnviado por el usuario: " +\
-                nombre + '[' + email + ']'
-            try: # FIXME: implementar correctamente la configuración de SMTP usando correo del administrador que está en base de datos
-                send_mail(asunto, mensaje, email, ['tsgdumbacc@gmail.com'])
-                send_mail("Mensaje recibido", "Agredecemos su aporte.", 'tsgdumbacc@gmail.com', [email])
-            except BadHeaderError:
-                return HttpResponse('Configuración de correo inválida.')
-            enviado = True
-    
-    notificacion = "¡Listo! gracias por su mensaje" # FIXME: agregar mensaje de confirmación
-    
+        pass
+
+    return render(request, 'contacto.html')
+
+@require_GET
+def contacto(request):
+    formulario = Formulario()
     empresa = get_object_or_404(Agency, agency_id='TSG')
-    
+
     preguntas = Pregunta.objects.all()
     antes = preguntas.filter(categoria = -1)
     durante = preguntas.filter(categoria = 0)
@@ -46,8 +46,6 @@ def contacto(request):
         'durante': durante,
         'despues': despues,
         'formulario': formulario,
-        'enviado': enviado,
-        'notificacion': notificacion,
     }
-    
+
     return render(request, 'contacto.html', contexto)
