cb533491b472562294a4919a6953b5f6acb2d18f
Jeancahu <jeancahu@gmail.com>
2021-10-31 19:23:22 -0600

    add: JS para ocultar o mostrar la sección de alertas en form

--- buses/contacto/static/contacto/js/main_contacto.js
+++ buses/contacto/static/contacto/js/main_contacto.js
@@ -2,6 +2,17 @@
 
 // Se hace query del formulario
 const formulario = document.querySelector('form');
+const form_alerts = formulario.getElementsByClassName("form_alerts")[0]; // .childNodes[1];
+
+// TODO agregar listener a cada input para validar los datos por aparte con JS
+
+formulario.addEventListener(
+    'change',
+    event => {
+        // Al introducir cosas en el Form se ocultan las alertas del formulario
+        form_alerts.classList.add("form_alerts_hide");
+    }
+);
 
 formulario.addEventListener(
     'submit',
@@ -20,11 +31,21 @@ formulario.addEventListener(
                 console.log(data.message);
                 formulario.reset();
 
-                // TODO success banner
+                // Success banner
+                form_alerts.classList.remove("form_alerts_hide");
+                form_alerts.querySelector("DIV").classList.remove("alert-danger");
+                form_alerts.querySelector("DIV").classList.add("alert-success");
+                form_alerts.querySelector("DIV").innerHTML="Formulario <strong>enviado</strong> correctamente! Gracias!";
+
+                // TODO presentar cuando hay errores del lado del servidor (5xx)
             })
             .catch((error) => {
                 // Notificar POST erróneo
                 console.error('Error:', error);
+                form_alerts.classList.remove("form_alerts_hide");
+                form_alerts.querySelector("DIV").classList.remove("alert-success");
+                form_alerts.querySelector("DIV").classList.add("alert-danger");
+                form_alerts.querySelector("DIV").innerHTML="Formulario <strong>no enviado</strong> debido a error de conectividad.";
             });
     }
 );
