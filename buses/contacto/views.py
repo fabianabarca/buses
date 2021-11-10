ab4e9d41af728bbfa68db5a643f35f6ad3571735
Jeancahu <jeancahu@gmail.com>
2021-11-09 23:34:15 -0600

    add: Telegram interface for contact form

--- buses/contacto/views.py
+++ buses/contacto/views.py
@@ -3,6 +3,10 @@ from django.core.mail import send_mail, BadHeaderError
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.views.decorators.http import require_GET, require_POST
 
+from django.conf import settings
+import json
+import requests
+
 # Create your views here.
 
 from rutas.models import Agency
@@ -11,21 +15,41 @@ from .form import Formulario
 
 @require_POST
 def post_contact_form (request):
+
     formulario = Formulario(request.POST)
-    if formulario.is_valid():
-        nombre = formulario.cleaned_data['nombre']
-        email = formulario.cleaned_data['email']
-        asunto = formulario.cleaned_data['asunto']
-        mensaje = formulario.cleaned_data['mensaje']
-        mensaje = mensaje + "\nEnviado por el usuario: " +\
-            nombre + '[' + email + ']'
+    if not formulario.is_valid():
+        return JsonResponse(status=500, data={"message": "Ha ocurrido un error, por favor intente nuevamente, gracias."})
 
-        try: # FIXME: implementar correctamente la configuración de SMTP usando correo del administrador que está en base de datos
-            send_mail(asunto, mensaje, email, ['tsgdumbacc@gmail.com'])
-            # send_mail("Mensaje recibido", "Agredecemos su aporte.", 'tsgdumbacc@gmail.com', [email]) # TODO remove
+    nombre = formulario.cleaned_data['nombre']
+    email = formulario.cleaned_data['email']
+    asunto = formulario.cleaned_data['asunto']
+    mensaje = formulario.cleaned_data['mensaje']
+    mensaje = mensaje + "\n\nEnviado por el usuario: " +\
+        nombre + "\nCorreo: " + '[' + email + ']'
 
-        except BadHeaderError:
-            return JsonResponse(status=500, data={"message": "Ha ocurrido un error, por favor intente más tarde, gracias."})
+    try:
+        telegram_token = settings.TELEGRAM_BOT_TOKEN
+        telegram_chat_id = settings.TELEGRAM_ADMIN_GROUP
+        res = requests.get('https://api.telegram.org/bot' + str(telegram_token) + '/getMe')
+        response = json.loads(res.text)
+
+        # Comprobar que es el bot de tsg
+        assert (response['result']['username'] == "tsgBusesBot")
+        res = requests.get('https://api.telegram.org/bot' + str(telegram_token) +
+                           '/sendMessage?chat_id='+str(telegram_chat_id)+
+                           '&text='+str(asunto)+"\n\n"+str(mensaje))
+        response = json.loads(res.text)
+        print(response)
+
+    except:
+        print("No telegram bot interface")
+
+    try: # FIXME: implementar correctamente la configuración de SMTP usando correo del administrador que está en base de datos
+        send_mail(asunto, mensaje, email, ['tsgdumbacc@gmail.com'])
+        # send_mail("Mensaje recibido", "Agredecemos su aporte.", 'tsgdumbacc@gmail.com', [email]) # TODO remove
+
+    except BadHeaderError:
+        return JsonResponse(status=500, data={"message": "Ha ocurrido un error, por favor intente más tarde, gracias."})
 
     return JsonResponse(status=200, data={"message": '<i class="fas fa-info-circle"></i> Formulario <strong>enviado</strong> correctamente! Gracias!'})
 
