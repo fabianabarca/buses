d52b93bb6e2ef5161f43b7e3c94b078a340616f2
Jeancahu <jeancahu@gmail.com>
2021-11-11 15:22:09 -0600

    chg: usar la key ya antes expuesta para desarrollo ya que no es útil en producción

--- buses/buses/settings.py
+++ buses/buses/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'todo'
+SECRET_KEY = 'ncvn$%5pvewazmo)m&6xck96ek!gq79d404=$ej#j457*$x@6_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
