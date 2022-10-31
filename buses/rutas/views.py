f313852b51d09bcae71305f0ae69afb882182e96
Gianfranco <gianfranco.bagnarello@ucr.ac.cr>
2022-10-30 21:55:25 -0600

    documentation: añadida documentación para funciones obtenerViajesDomingo y obatenerViajesSabado

--- buses/rutas/views.py
+++ buses/rutas/views.py
@@ -57,17 +57,27 @@ def obtenerViajesEntreSemana(route_id_array):
     horario_entresemana_1, ramales_entresemana_1 = Trip.objects.horario_y_ramales(route_id_array=route_id_array,service_id='entresemana',direction='1')
     return horario_entresemana_0, ramales_entresemana_0, horario_entresemana_1, ramales_entresemana_1
 
+'''
+@param: arreglo con los ids de la ruta
+@description: obtiene de GTFS el trip en formato .txt
+@returns: horarios de rutas sabado
+'''
 def obtenerViajesSabado(route_id_array):
     horario_sabado_0, ramales_sabado_0 = Trip.objects.horario_y_ramales(route_id_array=route_id_array,service_id='sabado',direction='0')
     horario_sabado_1, ramales_sabado_1 = Trip.objects.horario_y_ramales(route_id_array=route_id_array,service_id='sabado',direction='1')
     return horario_sabado_0, ramales_sabado_0, horario_sabado_1, ramales_sabado_1
 
+'''
+@param: arreglo con los ids de la ruta
+@description: obtiene de GTFS el trip en formato .txt
+@returns: horarios de rutas domingo
+'''
 def obtenerViajesDomingo(route_id_array):
     horario_domingo_0, ramales_domingo_0 = Trip.objects.horario_y_ramales(route_id_array=route_id_array,service_id='domingo',direction='0')
     horario_domingo_1, ramales_domingo_1 = Trip.objects.horario_y_ramales(route_id_array=route_id_array,service_id='domingo',direction='1')
     return horario_domingo_0, ramales_domingo_0, horario_domingo_1, ramales_domingo_1
 
-'''s
+'''
 @param: http request, url de la ruta
 @description: muestra la informacion de cada ruta, san gabriel o acosta-ramales
 @returns: render de la pagina con las rutas obtenidas
